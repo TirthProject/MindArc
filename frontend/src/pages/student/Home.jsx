2a2a9d84e8e559fee4ce5ebfd5eaf44409c61eb1
Tirth Panchori <panchoritirth483@gmail.com>
2025-09-28 18:21:12 +0530

    time pass

--- frontend/src/pages/student/Home.jsx
+++ frontend/src/pages/student/Home.jsx
@@ -1,7 +1,7 @@
 function HomeS() {
     return (
         <div>
-            <h1>Welcome to the Student Dashboard</h1>
+            <h1>Welcome to the Student efhj Dashboard</h1>
             <p>This is the home page for students. Use the navigation to explore the app.</p>
         </div>
     );
